29beb0cbb858ba82274c0a77e50b12462531db0d
Nyambi Blaise <prof.masterb.ise@gmail.com>
2023-06-29 21:08:04 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Example of how to initiate a payment using the fapshi Nodejs SDK
+// Before running this, make sure to have axios installed. Also, add your apiuser and apikey to your fapshi.js file
+const fapshi = require('./fapshi');
+
+(async function main(){
+    const payment = {
+        amount: 500, //fapshi
+        email: 'myuser@email.com',
+        externalId: '12345',
+        userId: 'abcde',
+        redirectUrl: 'https://mywebsite.com',
+        message: 'testing SDK nodejs',
+        // cardOnly: true
+    }
+    const resp = await fapshi.initiatePay(payment)
+    console.log(resp)
+})()
